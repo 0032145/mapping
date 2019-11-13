58a04b3df4580bacff8fa7e74a4f568f1311e1a8
Administrator <Administrator@koitt03a-PC>
2019-11-13 09:23:36 +0900

    자리바꿈

---
+++ src/com/mapp/bdi/controller/TestController.java
@@ -0,0 +1,34 @@
+package com.mapp.bdi.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.mapp.bdi.service.TestService;
+
+public class TestController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String uri = request.getRequestURI();
+		String cmd = uri.substring(6);
+		TestService ts = new TestService();
+		if("list".equals(cmd)) {
+			request.setAttribute("list",ts.getList());
+		}
+		String path = "/uri/target";
+		RequestDispatcher rd = request.getRequestDispatcher(path);
+		rd.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ src/com/mapp/bdi/controller/URIController.java
@@ -0,0 +1,33 @@
+package com.mapp.bdi.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class URIController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final String prefix = "/WEB-INF/view";
+	private static final String suffix = ".jsp";
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String uri = request.getRequestURI();
+		uri = prefix + uri.substring(4) + suffix;
+		String msg = (String)request.getAttribute("msg");
+		msg += ", URIController를 거침";
+		request.setAttribute("msg", msg);
+		RequestDispatcher rd = request.getRequestDispatcher(uri);
+		rd.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
